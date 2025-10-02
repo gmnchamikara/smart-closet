e57441f3ecdbc1acc100be4ffd4b85c650d10a2b
Nuwan Chamikara <www.gmnuwanchamikara@gmail.com>
2025-10-02 12:01:17 +0530

    final changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-red-400 text-lg font-bold">hello hello 1️⃣1️⃣</Text>
+      <Text className="text-green-800 text-lg font-bold">hello123 hello 1️⃣</Text>
     </View>
   );
 }
